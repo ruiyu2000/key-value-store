07d8d3f451edef1c86a5be495372ceafdd28e411
Rui Yu <rui.yu@vipps.no>
2021-05-08 12:48:38 +0200

    Add client CLI

--- client/main.go
+++ client/main.go
@@ -1,1 +1,57 @@
-package client
+package main
+
+import (
+	"crypto/tls"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func main() {
+	args := os.Args[1:]
+	var (
+		node    = args[0]
+		command = args[1]
+		key     = args[2]
+		method  string
+		body    io.Reader
+	)
+
+	switch command {
+	case "get":
+		method = http.MethodGet
+	case "set":
+		method = http.MethodPost
+		value := args[3]
+		body = strings.NewReader("value=" + value)
+	case "delete":
+		method = http.MethodDelete
+	}
+
+	request, err := http.NewRequest(method, fmt.Sprintf("%s/%s", node, key), body)
+	if err != nil {
+		return
+	}
+
+	if command == "set" {
+		request.Header.Set("Content-Type", "application/x-www-form-urlencoded; param=value")
+	}
+
+	c := http.Client{
+		Transport: &http.Transport{TLSClientConfig: &tls.Config{}},
+	}
+
+	response, err := c.Do(request)
+	if err != nil {
+		fmt.Printf("%+v", err)
+		return
+	}
+	defer response.Body.Close()
+
+	bytes, err := ioutil.ReadAll(response.Body)
+
+	fmt.Println(string(bytes))
+}
